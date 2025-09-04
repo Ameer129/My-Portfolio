d178824d724b511fb587353d20f4f3f6ca980fdf
ameer129 <ameershaik815@gmail.com>
2025-09-04 21:35:30 +0530

    Updated CV download link

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -49,7 +49,7 @@ const About = () => {
           </p>
           {/* Resume Button */}
           <a
-            href="https://drive.google.com/file/d/1qnlye_xA0fpZ5pNyZMatk3GYnICulS7q/view?usp=drivesdk"
+            href="https://drive.google.com/file/d/14XSP-XUZnev49Y3Wxd740NluoedgYJoL/view?usp=drivesdk"
             target="_blank"
             rel="noopener noreferrer"
             className="inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105"
